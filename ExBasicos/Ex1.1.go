31a0af19b3b221dbad660b89174c48dbe5ad0fea
Reyel <reyel.conceicao@bandtec.com.br>
2023-01-12 15:03:23 -0300

    Primeiros exercicios

---
+++ ExBasicos/Ex1.1.go
@@ -0,0 +1,17 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println(x, y, z)
+}
---
+++ ExBasicos/Ex1.2.go
@@ -0,0 +1,14 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+
+	fmt.Println(x, y, z)
+}
